39ce050179046d11f32eef6b44b03571dc998764
r-spiewak <63987228+r-spiewak@users.noreply.github.com>
2024-07-30 15:50:00 -0400

    Add clang-format

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.system import package_manager
 
 
 class cpp_templateRecipe(ConanFile):
@@ -27,6 +28,26 @@ class cpp_templateRecipe(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("cmake/[>=3.22.6]")
+
+    def system_requirements(self):
+        # This is a bad idea, since packages have different names on
+        # different systems...
+        #for package in ["clang-format"]:
+        #    for manager in [
+        #        package_manager.Apt(self),
+        #        package_manager.Yum(self),
+        #        package_manager.Dnf(self),
+        #        package_manager.Apk(self),
+        #        package_manager.Brew(self),
+        #        package_manager.PacMan(self),
+        #        package_manager.Chocolatey(self),
+        #        package_manager.Zypper(self),
+        #        package_manager.Pkg(self),
+        #        package_manager.PkgUtil(self),
+        #    ]:
+        #        manager.install([package])
+        packages = ["clang-format=1:14.0-55~exp2"]
+        package_manager.Apt(self).install(packages,recommends=True)
 
     def config_options(self):
         if self.settings.os == "Windows":
